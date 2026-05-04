153dc8e68be19f5f6c31de559efc5164912f3109
blongite <ethanphamxd@gmail.com>
2026-05-04 09:25:54 -0700

    Code updated, uses regular arrays now

--- ttt.c
+++ ttt.c
@@ -2,71 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-/*
-int TTTinput(char arr[3][3], int pos, char team) {
-    // Hard-coded because a tic-tac-toe box always only is a 3x3
-    //har arr[3][3];
-    int count = 0;
-
-    switch(pos) {
-        case 1:
-            arr[0][0] = team;
-            break;
-        case 2:
-            arr[0][1] = team;
-            break;
-        case 3:
-            arr[0][2] = team;
-            break;
-        case 4:
-            arr[1][0] = team;
-            break;
-        case 5:
-            arr[1][1] = team;
-            break;
-        case 6:
-            arr[1][2] = team;
-            break;
-        case 7:
-            arr[2][0] = team;
-            break;
-        case 8:
-            arr[2][1] = team;
-            break;
-        case 9:
-            arr[2][2] = team;
-            break;
-        default:
-            return -1;
-    }
-
-
-
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            if (!(inputarr[i][j] == 'X') || !(inputarr[i][j] == 'O'))
-                inputarr[i][j] = count;
-                count++;
-        }
-    }
-
-    inputarr = arr;
-
-    return 1;
-}
-*/
-
-//int TTTinput()
-
 int main(void) {
     // Initialize Tic-tac-toe 2D array & other variables
     int intsquare[9];
     int truesquare[9];
-    //char charsquare[9];
-    //int turn = 1;
-    //int count = 1;
     
-
     // Fill TTT arrays
     for (int i = 0; i < 9; i++) {
         intsquare[i] = i;
@@ -85,15 +25,6 @@ int main(void) {
     }
 
     printf(" \n");
-    /*
-    for (int i = 0; i < 9; i++) {
-        if ((i % 3) == 0) {
-            printf(" \n");   
-        }
-        printf(" %d", truesquare[i]); 
-    }
-    */
-    //printf(" \n");
 
     // Loop until turn 10 (or 9 if you insist)
     for (int turn = 1; turn < 10; turn++) {
@@ -101,8 +32,25 @@ int main(void) {
         if ((turn % 2) != 0) {
             int input;
             // Get input from player
-            printf("\nPick your position (Player X): ");
-            scanf("%d", &input);
+            do {
+                printf("\nPick your position (Player X): ");
+                scanf("%d", &input);
+                
+                if ((input > 9)) {
+                    printf("\nInvalid input! Choose a number between 1-8! (Too high!)");
+                    printf("\n Number Chosen: %d", input);
+                    continue;
+                }
+                else if ((input < 0)) {
+                    printf("\nInvalid input! Choose a number between 1-8! (Too low!)");
+                    printf("\n Number Chosen: %d", input);
+                }
+                else if ((truesquare[input] == 1) && (truesquare[input] == 2)) {
+                    printf("\nThat spot's taken!");
+                }
+                
+            } while ((input < 0) && (input > 9) && (truesquare[input] != 1) && (truesquare[input] != 2));
+                
             // Plug input into method
             truesquare[input] = 1;
             
@@ -112,8 +60,24 @@ int main(void) {
         if ((turn % 2) == 0) {
             int input;
             // Get input from player
-            printf("\nPick your position (Player O): ");
-            scanf("%d", &input);
+            
+            do {
+                printf("\nPick your position (Player X): ");
+                scanf("%d", &input);
+
+                if ((truesquare[input] == 1) && (truesquare[input] == 2)) {
+                    printf("\nThat spot's taken!");
+                }
+                else if ((input > 9)) {
+                    printf("\nInvalid input! Choose a number between 1-8! (Too high!)");
+                    printf("\n Number Chosen: %d", input);
+                }
+                else if ((input < 0)) {
+                    printf("\nInvalid input! Choose a number between 1-8! (Too low!)");
+                    printf("\n Number Chosen: %d", input);
+                }
+
+            } while ((truesquare[input] != 1) && (truesquare[input] != 2) && (input < 0) && (input > 9));
             // Plug input into method
             truesquare[input] = 2;
             
@@ -142,8 +106,6 @@ int main(void) {
             
     }
         
-        
-
         // Test print code for true square
         for (int i = 0; i < 9; i++) {
             if ((i % 3) == 0) {
